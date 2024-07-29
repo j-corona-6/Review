5d619325dce80f2e36b05deab327d51ae3993244
acorona <coronachen66@gmail.com>
2024-07-29 11:06:37 -0400

    removed products functionality

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -24,7 +24,6 @@ export default function App() {
           Home
         </Link>
         <Link to="/app/additional">Página de Configuración</Link>
-        <Link to="/app/products">Productos</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
